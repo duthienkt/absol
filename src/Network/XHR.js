6d9ce3ad81c0ec750fbc3247132aacbf308d3b3f
duthienkt <blueskythien2010@live.com>
2019-05-04 13:42:55 +0700

    + network

--- src/Network/XHR.js
+++ src/Network/XHR.js
@@ -32,7 +32,11 @@ XHR.getRequest = function (url, props, success, failure) {
     return new Promise(function (rs, rj) {
         var request = XHR.makeHttpObject();
         request.open("GET", url, true);
-        Object.assign(request, props);
+        if (typeof props == 'string')
+            request.responseType = props || '';
+        else if (props && (typeof props == 'object')) {
+            Object.assign(request, props);
+        }
         request.send(null);
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
@@ -42,9 +46,8 @@ XHR.getRequest = function (url, props, success, failure) {
                     rs(response);
                 }
                 else {
-
-                    rj(request.status);
                     failure && failure(request.status, request.statusText);
+                    rj(request.status);
                 }
             }
         };
@@ -54,5 +57,47 @@ XHR.getRequest = function (url, props, success, failure) {
         }
     });
 };
+
+
+XHR.postRepquest = function (url, bodyJson, props, success, failure) {
+    return new Promise(function (rs, rj) {
+        var method = "POST";
+        var shouldBeAsync = true;
+
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    success && success(request.response);
+                    rs(request.response);
+                }
+                else if (failure) {
+                    failure && failure(request.status, request.statusText);
+                    rj({ status: request.status, statusText: request.statusText });
+                }
+            }
+        };
+
+        request.onerror = function () {
+            var error = new Error("Network Error!");
+            if (failure) failure(error);
+            rj(error);
+        }
+
+        request.open(method, url, shouldBeAsync);
+        if (typeof props == 'string')
+            request.responseType = props || '';
+        else if (props && (typeof props == 'object')) {
+            Object.assign(request, props);
+        }
+
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        request.send(JSON.stringify(bodyJson));
+    });
+};
+
+
 
 export default XHR;
---
+++ src/Network/XLoader.js
@@ -0,0 +1,28 @@
+var XLoader = {};
+
+XLoader.loadScript = function (url, callback) {
+    return new Promise(function (resolve, reject) {
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+        if (script.readyState) {  //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" ||
+                    script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback && callback();
+                    resolve();
+                }
+            };
+        } else {  //Others
+            script.onload = function () {
+                callback && callback();
+                resolve();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
+};
+
+export default XLoader;
