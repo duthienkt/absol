b2fd1f266bede07794914088fa0bc5686fd33f37
duthienkt <blueskythien2010@live.com>
2019-05-08 17:29:03 +0700

    + file converter

---
+++ src/Converter/file.js
@@ -0,0 +1,31 @@
+export function blobToFile(theBlob, fileName) {
+    //A Blob() is almost a File() - it's just missing the two properties below which we will add
+    theBlob.lastModifiedDate = new Date();
+    theBlob.name = fileName;
+    return theBlob;
+}
+
+export function dataURItoBlob(dataURI) {
+    var byteString = atob(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], { type: mimeString });
+    return blob;
+
+}
+
+export function blobToArrayBuffer(blob) {
+    var fileReader = new FileReader();
+    return new Promise(function(rs) {
+        fileReader.onload = function(event) {
+            var arrayBuffer = event.target.result;
+            rs(arrayBuffer)
+        };
+        fileReader.readAsArrayBuffer(blob);
+    });
+}
+
