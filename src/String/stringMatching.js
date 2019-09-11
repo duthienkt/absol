c8cfded58d62cd2909149f8e6a05b3f616aee740
duthienkt <blueskythien2010@live.com>
2019-09-11 11:01:03 +0700

    - string matching

--- src/String/stringMatching.js
+++ src/String/stringMatching.js
@@ -1,15 +1,16 @@
+/**
+ * 
+ * @param {String} a 
+ * @param {String} b 
+ */
 export function wordLike(a, b) {
     var m = a.length;
     var n = b.length;
 
     function map(i, j) {
-        return i * m + j;
+        return i * (n + 1) + j;
     }
-    var Q = [];
-    for (var i = 0; i <= m; ++i)
-        Q[map(i, 0)] = 0.0;
-    for (var j = 0; j <= n; ++j)
-        Q[map(0, j)] = 0.0;
+    var Q = Array((m + 1) * (n + 1)).fill(0);
 
     for (var i = 0; i < m; ++i)
         for (var j = 0; j < n; ++j)
@@ -27,6 +28,40 @@ export function wordLike(a, b) {
     return Q[map(m, n)] / Math.max(m, n);
 }
 
+/**
+ * 
+ * @param {Array<String>} sq1 
+ * @param {Array<String>} sq2 
+ */
+export function wordsMatch(sq1, sq2, matchWordPow) {
+    matchWordPow = matchWordPow === undefined ? 1 : matchWordPow;
+
+    var m = sq1.length;
+    var n = sq2.length;
+    function map(i, j) {
+        return i * (n + 1) + j;
+    }
+    var Q = Array((m + 1) * (n + 1)).fill(0);
+    var e = 0.0;
+    for (var i = 0; i < m; ++i)
+        for (var j = 0; j < n; ++j) {
+            e = Math.pow(wordLike(sq1[i], sq2[j]), matchWordPow);
+
+            if (Q[map(i + 1, j + 1)]) {
+                if (Q[map(i + 1, j + 1)] < Q[map(i, j)] + e)
+                    Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
+
+            }
+            else
+                Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
+
+            e = Math.max(Q[map(i + 1, j)], Q[map(i, j + 1)]);
+            if (e > Q[map(i + 1, j + 1)]) Q[map(i + 1, j + 1)] = e;
+
+        }
+
+    return Q[map(m, n)];
+}
 
 
 export function phraseMatch(a, b, matchWordPow) {
@@ -35,36 +70,11 @@ export function phraseMatch(a, b, matchWordPow) {
     var notEmp = function (e) { return e.length > 0; };
     var sq1 = a.toLowerCase().split(spliter).filter(notEmp);
     var sq2 = b.toLowerCase().split(spliter).filter(notEmp);
-    var m = sq1.length;
-    var n = sq2.length;
-
-    function map(i, j) {
-        return i * m + j;
-    }
-    var Q = [];
-    for (var i = 0; i <= m; ++i)
-        Q[map(i, 0)] = 0.0;
-    for (var j = 0; j <= n; ++j)
-        Q[map(0, j)] = 0.0;
-
-    var e = 0.0;
-    for (var i = 0; i < m; ++i)
-        for (var j = 0; j < n; ++j) {
-            e = Math.pow(wordLike(sq1[i], sq2[j]), matchWordPow);
-            if (Q[map(i + 1, j + 1)]) {
-                if (Q[map(i + 1, j + 1)] < Q[map(i, j)] + e)
-                    Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
-
-            }
-            else
-                Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
-            e = Math.max(Q[map(i + 1, j)], Q[map(i, j + 1)]);
-            if (e > Q[map(i + 1, j + 1)]) Q[map(i + 1, j + 1)] = e;
-        }
-    return Q[map(m, n)] / Math.max(Math.min(m, n), 1);
+    wordsMatchScore = wordsMatch(sq1, sq2);
+    return wordsMatchScore / Math.max(Math.min(m, n), 1);
 };
 
-export function phraseLike (a, b, matchWordPow) {
+export function phraseLike(a, b, matchWordPow) {
     matchWordPow = matchWordPow || 1;
     var spliter = /[\s,-\.+?\_]+/;
     var notEmp = function (e) { return e.length > 0; };
@@ -72,30 +82,7 @@ export function phraseLike (a, b, matchWordPow) {
     var sq2 = b.toLowerCase().split(spliter).filter(notEmp);
     var m = sq1.length;
     var n = sq2.length;
-
-    function map(i, j) {
-        return i * m + j;
-    }
-    var Q = [];
-    for (var i = 0; i <= m; ++i)
-        Q[map(i, 0)] = 0.0;
-    for (var j = 0; j <= n; ++j)
-        Q[map(0, j)] = 0.0;
-
-    var e = 0.0;
-    for (var i = 0; i < m; ++i)
-        for (var j = 0; j < n; ++j) {
-            e = Math.pow(wordLike(sq1[i], sq2[j]), matchWordPow);
-            if (Q[map(i + 1, j + 1)]) {
-                if (Q[map(i + 1, j + 1)] < Q[map(i, j)] + e)
-                    Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
-
-            }
-            else
-                Q[map(i + 1, j + 1)] = Q[map(i, j)] + e;
-            e = Math.max(Q[map(i + 1, j)], Q[map(i, j + 1)]);
-            if (e > Q[map(i + 1, j + 1)]) Q[map(i + 1, j + 1)] = e;
-        }
-    return Q[map(m, n)] / Math.max(Math.max(m, n), 1);
+    wordsMatchScore = wordsMatch(sq1, sq2);
+    return wordsMatchScore / Math.max(Math.max(m, n), 1);
 }
 
