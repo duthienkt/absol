80c20e31b261c23e34113d1d5291d3b32f95296e
duthienkt <blueskythien2010@live.com>
2019-10-01 13:36:48 +0700

    + numberToString

--- src/Math/int.js
+++ src/Math/int.js
@@ -36,3 +36,96 @@ export function distance(x0, y0, x1, y1) {
     var dy = y0 - y1;
     return Math.sqrt(dx * dx + dy * dy);
 }
+
+/**
+ * 
+ * @param {Number} numb 
+ * @param {Number} floatFixed 
+ * @param {"."|","} decimalSeparator 
+ * @param {","|"."} thousandsSeparator 
+ * @param {Number} paddingDecimal 
+ */
+export function numberToString(numb, floatFixed, decimalSeparator, thousandsSeparator, paddingDecimal) {
+    if (floatFixed === undefined || floatFixed === null || typeof floatFixed != "number" || isNaN(floatFixed) || floatFixed < -1) floatFixed = -1;
+    if (decimalSeparator === undefined || decimalSeparator === null || (decimalSeparator != '.' && decimalSeparator != ',')) decimalSeparator = '.';
+    if (thousandsSeparator === undefined || thousandsSeparator === null || (floatFixed >= 0 && thousandsSeparator == decimalSeparator)) thousandsSeparator = undefined;
+    if (thousandsSeparator != ',' && thousandsSeparator != '.') thousandsSeparator = undefined;
+    if (paddingDecimal === undefined || paddingDecimal === null || typeof paddingDecimal != "number" || isNaN(paddingDecimal) || paddingDecimal < 0) paddingDecimal = 0;
+
+    var text = numb.toString();
+    var matched = text.match(/[+-]?([0-9]*)(\.([0-9]*))?(e([+-]?[0-9]+))?/);
+    var dec = matched[1] || '';
+    var real = matched[3] || '';
+    var floatPoint = parseInt(matched[5] || '0');
+    var decDigits = dec.split('').map(function (d) { return parseInt(d) });
+    var realDigits = real.split('').map(function (d) { return parseInt(d) });
+
+    while (floatPoint < 0) {
+        if (decDigits.length > 0) {
+            realDigits.unshift(decDigits.pop());
+        }
+        else {
+            realDigits.unshift(0);
+        }
+        floatPoint++;
+    }
+    while (floatPoint > 0) {
+        if (realDigits.length > 0) {
+            decDigits.push(realDigits.unshift());
+        }
+        else {
+            decDigits.push(0);
+        }
+        floatPoint++;
+    }
+
+    var mem = 0, i, cValue;
+    if (floatFixed > realDigits.length) {
+        while (realDigits.length < floatFixed) {
+            realDigits.push(0);
+        }
+    }
+    else if (floatFixed < realDigits.length && floatFixed >= 0) {
+        i = floatFixed;
+        mem = realDigits[i] >= 5 ? 1 : 0;
+        realDigits.splice(floatFixed);
+        --i;
+        while (mem > 0) {
+            if (i >= 0) {
+                cValue = realDigits[i] + mem;
+                realDigits[i] = cValue % 10;
+                mem = Math.floor(cValue / 10);
+            }
+            else {
+                if (decDigits.length + i < 0) decDigits.unshift(0);
+                cValue = decDigits[decDigits.length + i] + mem;
+                decDigits[decDigits.length + i] = cValue % 10;
+                mem = Math.floor(cValue / 10);
+            }
+            --i;
+        }
+    }
+
+
+    while (decDigits.length < paddingDecimal) {
+        decDigits.unshift(0);
+    }
+
+    var decText = numb < 0 ? '-' : '';
+    var breadMod = (decDigits.length + 2) % 3;
+    if (decimalSeparator != '') {
+        for (i = 0; i < decDigits.length; ++i) {
+            decText += decDigits[i];
+            if (i % 3 == breadMod && i + 1 < decDigits.length) {
+                decText += thousandsSeparator;
+            }
+        }
+    }
+    else {
+        decText += decDigits.join('');
+    }
+
+    var realText = realDigits.length == 0 ? '' : decimalSeparator + realDigits.join('');
+
+    return decText + realText;
+}
