8607021104ecc88692fbb8637b2fc843241ab911
duthienkt <blueskythien2010@live.com>
2020-06-05 08:18:16 +0700

    * miss prototype

--- src/HTML5/DomSignal.js
+++ src/HTML5/DomSignal.js
@@ -1,14 +1,19 @@
 import Dom from "./Dom";
+import EventEmitter from "./EventEmitter";
 
 /**
  * Emit event with dom delay
  */
 function DomSignal() {
+    EventEmitter.call(this);
     this.signals = {};
     this.ev_error = this.ev_error.bind(this);
     this.$attachhook = null;
-
 }
+
+Object.defineProperties(DomSignal.prototype, Object.getOwnPropertyDescriptors(EventEmitter.prototype));
+DomSignal.prototype.constructor = DomSignal;
+
 
 DomSignal.prototype.execSignal = function () {
     var signals = this.signals;
