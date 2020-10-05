9b49163d115cb5db19acfa17741f7b31818e6a19
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-10-05 11:58:11 +0700

    - transpose

--- src/Math/Mat3.js
+++ src/Math/Mat3.js
@@ -154,6 +154,25 @@ Mat3.prototype.scale = function (v) {
     return new Mat3(out);
 };
 
+/***
+ *
+ * @return {Mat3}
+ */
+Mat3.prototype.transpose = function (){
+    var a = this.data;
+    var out = Array(9);
+    out[0] = a[0];
+    out[1] = a[3];
+    out[2] = a[6];
+    out[3] = a[1];
+    out[4] = a[4];
+    out[5] = a[7];
+    out[6] = a[2];
+    out[7] = a[5];
+    out[8] = a[8];
+    return new Mat3(out);
+};
+
 /**
  *
  * @param {Vec2}v
