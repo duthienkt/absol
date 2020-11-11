66197e82a3b341b779631552f86cf64df55db9bf
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-11 17:46:21 +0700

    * special key

--- src/Input/keyboard.js
+++ src/Input/keyboard.js
@@ -1,10 +1,25 @@
-
 export function isFnKey(name) {
     return name.match(/^[fF]([2-9]|(1([0-2]?)))$/)
 }
 
 export function isSpaceKey(name) {
     return name === ' ' || name.match(/^[sS]pace$/);
+}
+
+export function isCtrlKey(name) {
+    return name.match(/(^([cC]ontrol)|ctrl)$/);
+}
+
+export function isAltKey(name) {
+    return name.match(/^[aA]lt$/);
+}
+
+export function isShiftKey(name) {
+    return name.match(/^[sS]hift$/);
+}
+
+export function isMetaKey(name) {
+    return name.toLowerCase().match(/^(command|windows|meta)$/);
 }
 
 /***
@@ -18,9 +33,10 @@ export function normalizeKeyBindingIdent(text) {
             return w.length > 0;
         });
     var values = {
-        ctrl: 1,
-        alt: 2,
-        shift: 3
+        meta: 1,
+        ctrl: 2,
+        alt: 3,
+        shift: 4
     };
     keys.sort(function (a, b) {
         var va, vb;
@@ -38,18 +54,23 @@ export function normalizeKeyBindingIdent(text) {
  */
 export function keyboardEventToKeyBindingIdent(event) {
     var keys = [];
+    if (event.metaKey) {
+        keys.push('meta');
+    }
     if (event.ctrlKey)
         keys.push('ctrl');
     if (event.altKey)
         keys.push('alt');
     if (event.shiftKey)
         keys.push('shift');
+
     if (isSpaceKey(event.key)) {
         keys.push('space');
     }
     else if (isFnKey(event.key)) {
         keys.push(event.key.toLowerCase())
     }
-    else keys.push(event.key.toLowerCase());
+    else if (!isMetaKey(event.key) && !isAltKey(event.key) && isCtrlKey(event.key) && isShiftKey(event.key))
+        keys.push(event.key.toLowerCase());
     return keys.join('-');
 }
