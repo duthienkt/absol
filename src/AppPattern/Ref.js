9ccb9698e880c4c328b0055ba74f69e8a3ec16f4
duthienkt <blueskythien2010@live.com>
2021-08-29 21:36:47 +0700

    - document

--- src/AppPattern/Ref.js
+++ src/AppPattern/Ref.js
@@ -1,3 +1,9 @@
+/***
+ *
+ * @param {T} value
+ * @constructor
+ * @template T
+ */
 function Ref(value) {
     this.set(value);
 }
@@ -6,19 +12,32 @@ Ref.prototype.toString = function () {
     return this.value + '';
 };
 
+/***
+ *
+ * @return {T}
+ */
 Ref.prototype.valueOf = function () {
     return this.value;
 };
 
+/***
+ *
+ * @param {T}value
+ */
 Ref.prototype.set = function (value) {
     this.value = value;
 };
 
+/**
+ *
+ * @return {T}
+ */
 Ref.prototype.get = function () {
     return this.value;
 }
 
 export default Ref;
+
 
 
 
