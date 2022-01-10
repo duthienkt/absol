07d553b1ed13b958e7cc626fa39914c1adb23f34
duthienkt <blueskythien2010@live.com>
2022-01-10 20:27:35 +0700

    + array utils

---
+++ src/DataStructure/Array.js
@@ -0,0 +1,15 @@
+/***
+ *
+ * @param {(string|number|boolean|null)[]} arr
+ * @returns {(string|number|boolean|null)[]}
+ */
+export function uniqueArray(arr) {
+    var dict = {};
+    return arr.reduce(function (ac, cr) {
+        var key = (typeof cr) + '//' + cr;
+        if (key in dict) return ac;
+        ac.push(cr);
+        dict[key] = true;
+        return ac;
+    }, []);
+}
