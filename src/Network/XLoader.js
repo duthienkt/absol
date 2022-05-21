ff9023e1da6912cb4b56a14462b0c6b743b08d78
duthienkt <blueskythien2010@live.com>
2022-05-21 10:54:58 +0700

    - error event

--- src/Network/XLoader.js
+++ src/Network/XLoader.js
@@ -1,28 +1,41 @@
 var XLoader = {};
 
-XLoader.loadScript = function (url, callback) {
+export function loadScript(url, onComplete, onError) {
     return new Promise(function (resolve, reject) {
         var script = document.createElement("script");
         script.type = "text/javascript";
         if (script.readyState) {  //IE
             script.onreadystatechange = function () {
-                if (script.readyState == "loaded" ||
-                    script.readyState == "complete") {
+                if (script.readyState === "loaded" ||
+                    script.readyState === "complete") {
                     script.onreadystatechange = null;
-                    callback && callback();
+                    onComplete && onComplete();
                     resolve();
                 }
             };
-        } else {  //Others
+        }
+        else {  //Others
             script.onload = function () {
-                callback && callback();
+                script.onload = null;
+                script.onerror = null;
+                onComplete && onComplete();
                 resolve();
             };
+
+            script.onerror = function () {
+                script.onerror = null;
+                script.onload = null;
+                script.remove();
+                onError && onError();
+                reject();
+            }
         }
 
         script.src = url;
         document.getElementsByTagName("head")[0].appendChild(script);
     });
-};
+}
+
+XLoader.loadScript = loadScript;
 
 export default XLoader;
