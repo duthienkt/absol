5b13fc59b090aa8e15e10c3f04755acff63aef55
duthienkt <blueskythien2010@live.com>
2022-09-11 17:47:10 +0700

    - detect zoom(not tested)

--- src/Detector/BrowserDetector.js
+++ src/Detector/BrowserDetector.js
@@ -12,6 +12,7 @@ function BrowserDetector(rulesheet) {
     this.device = this.detectByRules(this.rulesheet.device);
     this.engine = this.detectByRules(this.rulesheet.engine);
     this.browser = this.detectByRules(this.rulesheet.browser);
+
     this.isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
     this.isCococ = navigator.userAgent.toLowerCase().indexOf('coc_coc_browser') >= 1;
     this.isSafari = !this.isCococ && navigator.userAgent.toLowerCase().indexOf('safari') > -1
@@ -41,6 +42,14 @@ function BrowserDetector(rulesheet) {
         return supportsPassiveOption;
     })();
     this.supportGridLayout = typeof document.createElement('div').style.grid === 'string';
+
+    Object.defineProperty(this, 'zoom', {
+        get: function () {
+            return this.getZoom();
+        },
+        enumerable: true,
+        configurable: false
+    });
 }
 
 
@@ -70,6 +79,59 @@ BrowserDetector.prototype.detectByRules = function (rules) {
         }
     }
     return result;
+};
+
+
+BrowserDetector.prototype.getZoom = function () {
+    //todo: wrong on chrome
+    var type;
+    if ('chrome' in window) {
+        type = "chrome";
+    }
+    else if (this.isSafari) {
+        type = 'safari';
+    }
+    else if ('orientation' in window && 'webkitRequestAnimationFrame' in window){
+        type = 'webkitMobile';
+    }
+    else if ('webkitRequestAnimationFrame' in window){
+        type = 'webkit';
+    }
+
+    switch (type) {
+        case 'chrome':
+            return Math.round(((window.outerWidth) / window.innerWidth) * 100) / 100;
+        case 'safari':
+            return Math.round(((document.documentElement.clientWidth) / window.innerWidth) * 100) / 100;
+        case 'webkitMobile':
+            return ((Math.abs(window.orientation) == 90) ? screen.height : screen.width) / window.innerWidth;
+        case 'webkit':
+            return (() => {
+                var important = (str) => {
+                    return str.replace(/;/g, " !important;");
+                };
+
+                var div = document.createElement('div');
+                div.innerHTML = "1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>0";
+                div.setAttribute('style', important('font: 100px/1em sans-serif; -webkit-text-size-adjust: none; text-size-adjust: none; height: auto; width: 1em; padding: 0; overflow: visible;'));
+
+                var container = document.createElement('div');
+                container.setAttribute('style', important('width:0; height:0; overflow:hidden; visibility:hidden; position: absolute;'));
+                container.appendChild(div);
+
+                document.body.appendChild(container);
+                var zoom = 1000 / div.clientHeight;
+                zoom = Math.round(zoom * 100) / 100;
+                document.body.removeChild(container);
+
+                return zoom;
+            })();
+        default:
+            return 1;
+
+    }
+
+    return 1;
 };
 
 export function calcBenchmark() {
