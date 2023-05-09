6eecfed44de706ecfc2397fbc16b6a1a5acdc7e0
duthienkt <blueskythien2010@live.com>
2023-05-09 14:42:46 +0700

    * wait attached in chrome v113

--- src/HTML5/AttachHook.js
+++ src/HTML5/AttachHook.js
@@ -1,14 +1,18 @@
+import BrowserDetector from "../Detector/BrowserDetector";
+
 /***
  * @extends AElement
  * @constructor
  */
 export function AttachHook() {
+    this._attached = false;
     this.on('error', function (event) {
         if (!this._attached && this.isDescendantOf(document.body)) {
             this._attached = true;
             this.emit('attached', event, this);
         }
     });
+    this.waitAttaching();
 }
 
 AttachHook.render = function (data, domInstance) {
@@ -31,19 +35,36 @@ AttachHook.render = function (data, domInstance) {
             display: 'none'
         },
         attr: attributes,
-        props:{domInstance: domInstance}
+        props: { domInstance: domInstance }
     });
+};
+
+AttachHook.prototype.waitAttaching = function () {
+    var self = this;
+    if (BrowserDetector.browser.type.startsWith('chrome') && parseInt((BrowserDetector.browser.version || '').split('.').shift()) >= 113) {
+        if (this.waitTimeout > 0) clearTimeout(this.waitTimeout);
+        this.waitTimeout = setTimeout(function wait() {
+            self.waitTimeout = -1;
+            if (!self._attached && self.isDescendantOf(document.body)) {
+                self._attached = true;
+                self.emit('attached', { target: this }, self);
+            }
+            else if (!self._attached) {
+                self.waitTimeout = setTimeout(wait, 10);
+            }
+        }, 0);
+    }
 };
 
 AttachHook.prototype.resetState = function () {
     this._attached = false;
-    if (this.tagName.toLowerCase() === 'img' ){
+    if (this.tagName.toLowerCase() === 'img') {
         this.src = '';
-
     }
     else {
         this.href = '';
     }
+    this.waitAttaching();
 };
 
 AttachHook.property = {
