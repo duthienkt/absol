4fb2ad6dd3ba0c66092ae6ba9d75e690dda19109
duthienkt <blueskythien2010@live.com>
2024-11-20 23:13:21 +0700

    - catch readonly error

--- src/Code/safeThrow.js
+++ src/Code/safeThrow.js
@@ -1,7 +1,12 @@
 export default function safeThrow(error) {
     setTimeout(function () {
         if (error.stack) {
-            error.message += '\n' + error.stack;
+            try {
+                error.message += '\n' + error.stack;
+            }
+            catch (e){
+                //can not modify message
+            }
         }
         throw error;
     }, 0);
